9a0fedcb72607c17c873e2299efd59decaf72bee
TabGuima <tab.guimaraes@gmail.com>
2025-02-24 11:09:06 -0300

    criacao de metodos classe paciente

--- src/br/sp/senai/jandira/clinica/ClinicaApp.java
+++ src/br/sp/senai/jandira/clinica/ClinicaApp.java
@@ -1,5 +1,7 @@
 package br.sp.senai.jandira.clinica;
 
+import java.time.LocalDate;
+
 import br.sp.senai.jandira.clinica.model.Paciente;
 
 public class ClinicaApp {
@@ -8,11 +10,14 @@ public class ClinicaApp {
 		
 		Paciente p1 = new Paciente();
 		
-		p1.setNome("Jó");
+		p1.setNome("Joana");
 		p1.setPeso(68);
 		p1.setAltura(160);		
+		p1.setGenero("F");
+		p1.setTelefone("9998-5746");
+		p1.setDataNascimento(LocalDate.of(2004, 2, 29));
 		p1.exibirDados();
-		
+			// System.out.println(p1.getNome());
 				
 	}
 
--- src/br/sp/senai/jandira/clinica/model/Paciente.java
+++ src/br/sp/senai/jandira/clinica/model/Paciente.java
@@ -1,89 +1,131 @@
 package br.sp.senai.jandira.clinica.model;
 
 import java.time.LocalDate;
+// import java.time.format.DateTimeFormatter;
 
 public class Paciente {
-	   	private String nome;
-	   	private double peso;
-	   	private double altura;
-	    private String genero;
-	    private String telefone;
-	    private LocalDate dataNascimento;
-	    
-	    public void setNome (String nome) {
-	    	if (nome.length() <= 3 | nome.isBlank()) {
-	    		System.out.println("O nome não pode estar vazio ou ter menos que 3 caracteres!");
-	    	} else {
-	    		this.nome = nome;
-	    	}
-	    	
-	    }
-	    
-	    
-	    public void setPeso (double peso) {
-	    	if (peso > 0) {
-	    		this.peso = peso;
-	    	} else {
-	    		System.out.println("O peso deve ser maior que zero!");
-	    	}
-	    }
-	    
-	    public double getPeso () {
-	    	return this.peso;
-	    }
-	    
-	    
-	    public void setAltura (double altura) {
-	    	if (altura > 1) {
-	    		this.altura = altura;
-	    	} else {
-	    		System.out.println("A altura deve ser maior que zero!");
-	    	}
-	    }
-	    
-	    public double getAltura () {
-	    	return this.altura;
-	    }
-	    
+	private String nome;
+	private double peso;
+	private double altura;
+	private String genero;
+	private String telefone;
+	private LocalDate dataNascimento;
+	// DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
-	    public void marcarConsulta(){
+	public void setNome(String nome) {
+		if (nome.length() <= 3 | nome.isBlank()) {
+			System.out.println("O nome não pode estar vazio ou ter menos que 3 caracteres!");
+		} else {
+			this.nome = nome;
+		}
 
-	    }
+	}
 
-	    public void calcularIdade(){
+	public String getNome() {
+		return this.nome;
+	}
 
-	    }
+	public void setPeso(double peso) {
+		if (peso > 0) {
+			this.peso = peso;
+		} else {
+			System.out.println("O peso deve ser maior que zero!");
+		}
+	}
 
-	    public void calcularImc(){
-	    	
-	    	double	imc = this.peso / (this.altura * this.altura);
-	    	
+	public double getPeso() {
+		return this.peso;
+	}
 
-	    }
+	public void setAltura(double altura) {
+		if (altura > 1) {
+			this.altura = altura;
+		} else {
+			System.out.println("A altura deve ser maior que zero!");
+		}
+	}
 
-	    public void classificarImc(){
+	public double getAltura() {
+		return this.altura;
+	}
 
-	    }
+	public void setGenero(String genero) {
+		switch (genero) {
+		case "M":
+			this.genero = "Masculino";
+			break;
 
-	    public void exibirDados(){
-	    	
-	    	String unidadePeso = "kg. \n";
-	    	String separadorCampos = "===================";
-	    	System.out.println(separadorCampos);
-	    	System.out.println("DADOS DO PACIENTE");
-	    	System.out.println(separadorCampos);
-	    	if(this.nome != null) {
-	    		System.out.println(("nome: " + nome).toUpperCase());
-	    	} else {
-	    		System.out.println("nome: *****".toUpperCase());
-	      	}	    	
-	    	System.out.println(separadorCampos);	    	System.out.printf("PESO: %s %s", peso, unidadePeso);
-	    	System.out.println(separadorCampos);
-	   	    System.out.println("ALTURA: " + altura);
-	   	    System.out.println(separadorCampos);
-	    	
-		
+		case "F":
+			this.genero = "Feminino";
+			break;
+		default:
+			System.out.println("===================");
+			System.out.println("O genero deve ser preenchido como 'M' para masculino ou 'F' para feminino");
+		}
 
-	    }
+	}
+
+	public String getGenero() {
+		return this.genero;
+	}
+
+	public void setTelefone(String telefone) {
+		if (telefone.length() < 8 | telefone.isBlank()) {
+			System.out.println("O campo telefone não pode estar vazio ou ter menos que 8 caracteres!");
+		} else {
+			this.telefone = telefone;
+		}
+	}
+
+	public String getTelefone() {
+		return this.telefone;
+	}
+
+	
+ 	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	
+	public void marcarConsulta() {
+
+	}
+
+	public void calcularIdade() {
+
+	}
+
+	public void calcularImc() {
+
+		double imc = this.peso / (this.altura * this.altura);
+
+	}
+
+	public void classificarImc() {
+
+	}
+
+	public void exibirDados() {
+
+		String unidadePeso = "kg. \n";
+		String separadorCampos = "===================";
+		System.out.println(separadorCampos);
+		System.out.println("DADOS DO PACIENTE");
+		System.out.println(separadorCampos);
+		if (this.nome != null) {
+			System.out.println(("nome: " + nome).toUpperCase());
+		} else {
+			System.out.println("nome: *****".toUpperCase());
+		}
+
+		System.out.println(separadorCampos);
+		System.out.printf("PESO: %s %s", peso, unidadePeso);
+		System.out.println(separadorCampos);
+		System.out.println("ALTURA: " + altura);
+		System.out.println(separadorCampos);
+		System.out.println(("Genero: " + genero).toUpperCase());
+		System.out.println(separadorCampos);
+		System.out.println(("Telefone: " + telefone).toUpperCase());
+
+	}
 
 }
